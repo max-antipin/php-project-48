88d58ad340366028e5c66dd0c9513f990760484f
Max Antipin <max.v.antipin@gmail.com>
2025-12-16 08:47:19 +0300

    Nested diffs

--- src/Library.php
+++ src/Library.php
@@ -6,12 +6,12 @@ namespace Differ\Differ;
 
 use Symfony\Component\Yaml\Yaml;
 
-function decodeFile(string $filename): array
+function decodeFile(string $filename): object
 {
     $ext = pathinfo($filename, PATHINFO_EXTENSION);
     $parser = match ($ext) {
-        'json' => static fn (string $contents): mixed => json_decode($contents, true),
-        'yaml', 'yml' => static fn (string $contents): mixed => Yaml::parse($contents),
+        'json' => static fn (string $contents): mixed => json_decode($contents, false),
+        'yaml', 'yml' => static fn (string $contents): mixed => Yaml::parse($contents, Yaml::PARSE_OBJECT_FOR_MAP),
         default => throw new \RuntimeException("Unsupported file type '$ext'")
     };
     $contents = file_get_contents($filename);
@@ -19,7 +19,7 @@ function decodeFile(string $filename): array
         throw new \RuntimeException("Unable to open file '$filename'");
     }
     $data = $parser($contents);
-    if (!\is_array($data)) {
+    if (!\is_object($data)) {
         throw new \RuntimeException("Invalid file format '$filename'");
     }
     return $data;
@@ -27,36 +27,54 @@ function decodeFile(string $filename): array
 
 function genDiff(string $filename1, string $filename2): string
 {
+    return formatDiff(
+        calcDiff(
+            decodeFile($filename1),
+            decodeFile($filename2),
+        )
+    );
+}
+
+function calcDiff(object $data1, object $data2): array
+{
+    $allKeys = array_keys([...get_object_vars($data1), ...get_object_vars($data2)]);
+    sort($allKeys, SORT_STRING);
+    $diff = array_fill_keys($allKeys, []);
+    array_walk($diff, static function (array &$item, string $key) use ($data1, $data2): void {
+        $item = match ((property_exists($data1, $key) ? 1 : 0) + (property_exists($data2, $key) ? 2 : 0)) {
+            1 => ['-' => \is_object($data1->$key) ? calcDiff($data1->$key, $data1->$key) : $data1->$key],
+            2 => ['+' => \is_object($data2->$key) ? calcDiff($data2->$key, $data2->$key) : $data2->$key],
+            3 => (function (object $data1, object $data2, string $key): array {
+                $leftObj = \is_object($data1->$key);
+                $rightObj = \is_object($data2->$key);
+                $bothObj = $leftObj && $rightObj;
+                if ($data1->$key === $data2->$key || $bothObj) {
+                    return [' ' => $bothObj ? calcDiff($data1->$key, $data2->$key) : $data1->$key];
+                }
+                return [
+                    '-' => $leftObj ? calcDiff($data1->$key, $rightObj ? $data2->$key : $data1->$key) : $data1->$key,
+                    '+' => $rightObj ? calcDiff($data2->$key, $leftObj ? $data1->$key : $data2->$key) : $data2->$key,
+                ];
+            })($data1, $data2, $key)
+        };
+    });
+    return $diff;
+}
+
+function formatDiff(array $diff, int $level = 0): string
+{
     $formatLine = static fn(
             string $sign,
-            string $key,
-            string|int|float|bool $value
-        ): string => "$sign $key: " . (\is_string($value) ? $value : var_export($value, true));
-    $file1 = decodeFile($filename1);
-    $file2 = decodeFile($filename2);
-    $allKeys = array_keys([...$file1, ...$file2]);
-    sort($allKeys, SORT_STRING);
+            string $k,
+            null|string|int|float|bool $v
+        ): string => "$sign $k: " . (null === $v ? 'null' : (\is_string($v) ? $v : var_export($v, true)));
     $lines = [];
-    foreach ($allKeys as $key) {
-        switch ((isset($file1[$key]) ? 1 : 0) + (isset($file2[$key]) ? 2 : 0)) {
-            case 1:
-                $lines[] = $formatLine('-', $key, $file1[$key]);
-                break;
-            case 2:
-                $lines[] = $formatLine('+', $key, $file2[$key]);
-                break;
-            case 3:
-                if ($file1[$key] === $file2[$key]) {
-                    $lines[] = $formatLine(' ', $key, $file1[$key]);
-                } else {
-                    $lines[] = $formatLine('-', $key, $file1[$key]);
-                    $lines[] = $formatLine('+', $key, $file2[$key]);
-                }
-                break;
-            default:
-                die('Impossible');
+    foreach ($diff as $key => $d) {
+        foreach ($d as $sign => $value) {
+            $lines[] = $formatLine($sign, $key, \is_array($value) ? formatDiff($value, $level + 1) : $value);
         }
     }
-    $diff = implode("\n", array_map(static fn (string $s): string => "  $s", $lines));
-    return "{\n$diff\n}";
+    $offset = str_repeat(' ', 4 * $level);
+    $s = implode("\n", array_map(static fn (string $s): string => "$offset  $s", $lines));
+    return "{\n$s\n$offset}";
 }
