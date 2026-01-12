cba094925fd458ef40776901789e42b02d61a285
Max Antipin <max.v.antipin@gmail.com>
2026-01-12 05:06:48 +0300

    Use first class callable syntax

--- src/Formatters.php
+++ src/Formatters.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Differ\Differ;
 
 use Differ\Differ\Enum\Format;
+use Differ\Differ\Formatters;
 
 function getFormatter(string $format): callable
 {
-    return __NAMESPACE__ . '\\Formatters\\' . match ($format) {
-        Format::STYLISH->value => 'formatDiffStylish',
-        Format::PLAIN->value => 'formatDiffPlain',
-        Format::JSON->value => 'formatDiffJson',
+    return match ($format) {
+        Format::STYLISH->value => Formatters\formatDiffStylish(...),
+        Format::PLAIN->value => Formatters\formatDiffPlain(...),
+        Format::JSON->value => Formatters\formatDiffJson(...),
         default => throw new \UnexpectedValueException(
             "Invalid format name: $format; acceptable names: " . implode(', ', array_column(Format::cases(), 'value'))
         )
