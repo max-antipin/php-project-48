056eaf326168a8e66a1fc1dd1f27c07c8d8a02c5
Max Antipin <max.v.antipin@gmail.com>
2026-01-12 04:43:06 +0300

    Add Parsers.php, rename Library.php

--- src/Differ.php
+++ src/Differ.php
@@ -5,23 +5,6 @@ declare(strict_types=1);
 namespace Differ\Differ;
 
 use Differ\Differ\Enum\Format;
-use Symfony\Component\Yaml\Yaml;
-
-function getContentParser(string $filename): callable
-{
-    $ext = pathinfo($filename, PATHINFO_EXTENSION);
-    $check = static function (mixed $data) use ($filename): object {
-        if (!\is_object($data)) {
-            throw new \RuntimeException("Invalid file format '$filename'");
-        }
-        return $data;
-    };
-    return match ($ext) {
-        'json' => static fn (string $s): object => $check(json_decode($s, false)),
-        'yaml', 'yml' => static fn (string $s): object => $check(Yaml::parse($s, Yaml::PARSE_OBJECT_FOR_MAP)),
-        default => throw new \RuntimeException("Unsupported file type '$ext'")
-    };
-}
 
 function readFile(string $filename): string
 {
